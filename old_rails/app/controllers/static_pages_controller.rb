8ca5e2aa7b6dd2c9963533346478f69c3f0b62b1
catcher1 <danlau168@gmail.com>
2015-11-26 12:28:41 -0800

    nodify app

--- old_rails/app/controllers/static_pages_controller.rb
+++ old_rails/app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   def root
 
      @instagram = ::Instagram.user_recent_media(2290945337, {:count => 2})
-     render :json => @instagram
+     render :json 
      end
   end
