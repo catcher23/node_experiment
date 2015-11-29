250edfbfe1050b4f0db746025a4b3909a36ba6e5
Danny <danlau168@gmail.com>
2015-11-29 01:48:56 -0800

    clementine setup

---
+++ server.js
@@ -0,0 +1,13 @@
+/*jslint node: true */
+'use strict';
+
+var express = require('express');
+  var routes = require('./app/routes/index.js');
+
+var app = express();
+
+routes(app);
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000...');
+});
