7e16ad297d856a051a3d582371e73fab80f85f6e
Danny <danlau168@gmail.com>
2015-11-29 18:05:58 -0800

    set up mongolab

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -5,7 +5,7 @@
    var addButton = document.querySelector('.btn-add');
    var deleteButton = document.querySelector('.btn-delete');
    var clickNbr = document.querySelector('#click-nbr');
-   var apiUrl = 'http://localhost:3000/api/clicks';
+   var apiUrl = process.cwd()+ '/api/clicks';
 
    function ready (fn) {
       if (typeof fn !== 'function') {
