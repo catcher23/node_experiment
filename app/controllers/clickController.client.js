405c62249deb549318bdfb47f5e2f4701c17465e
Danny <danlau168@gmail.com>
2015-11-29 16:38:16 -0800

    deploy heroku

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -5,7 +5,7 @@
    var addButton = document.querySelector('.btn-add');
    var deleteButton = document.querySelector('.btn-delete');
    var clickNbr = document.querySelector('#click-nbr');
-   var apiUrl = 'http://localhost:3000/api/clicks';
+   var apiUrl = 'https://safe-cliffs-9033.herokuapp.com/api/clicks';
 
    function ready (fn) {
       if (typeof fn !== 'function') {
